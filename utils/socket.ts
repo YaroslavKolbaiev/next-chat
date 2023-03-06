e7850e5e127c019e15c2fabd9804cbd5e113773a
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2023-03-06 16:50:44 +0300

    fix: socket

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,6 +1,5 @@
 import { io } from 'socket.io-client';
 
-const ENDPOINT = 'http://localhost:5000';
-const ENDPOINT_DEVELOP = 'https://chatic.herokuapp.com';
+const ENDPOINT = 'https://chatic.herokuapp.com';
 
 export const socket = io(ENDPOINT);
