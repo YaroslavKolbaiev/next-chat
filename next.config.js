6acd0f42cee0f59eb9d40affdaec2aa9c7cfa592
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2023-04-11 11:45:26 +0300

    fix: endpoits

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     HOST_DEVELOP: "http://localhost:5000",
-    HOST: "https://chatic.herokuapp.com",
+    HOST: "https://chatic.onrender.com",
   },
 }
 
--- utils/socket.ts
+++ utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const ENDPOINT = 'https://chatic.herokuapp.com';
+const ENDPOINT = 'https://chatic.onrender.com';
 // const ENDPOINT_DEVELOP = 'http://localhost:5000';
 
 export const socket = io(ENDPOINT);
