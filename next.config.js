9f98feacfcd6088dc5f718ea8a818a3968cbceef
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2023-12-07 10:44:34 +0300

    Update next.config.js
    replace new server url with gcloud

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     HOST_DEVELOP: "http://localhost:5000",
-    HOST: "https://chatic.onrender.com",
+    HOST: "https://chat-app-service-vsdq62nw3a-lm.a.run.app",
   },
 }
 
